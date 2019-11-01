69ab75575bde95ab89877ce28aea3025093894c5
Udit Prabhakar <uditprabhakar.up@gmail.com>
2019-11-02 00:34:34 +0530

    Sign In and Sign Up Working Properly With Firebase Authentication.

--- app/src/main/java/in/ac/sharda/chicagopizza/login.java
+++ app/src/main/java/in/ac/sharda/chicagopizza/login.java
@@ -1,15 +1,77 @@
 package in.ac.sharda.chicagopizza;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class login extends AppCompatActivity {
+    EditText txtEmail, txtPassword;
+    Button btn_login;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        txtEmail=(EditText)findViewById(R.id.loginemail);
+        txtPassword=(EditText)findViewById(R.id.loginpassword);
+        btn_login=(Button)findViewById(R.id.signinBtn);
+
+        firebaseAuth=firebaseAuth.getInstance();
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String email=txtEmail.getText().toString().trim();
+                String password=txtPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(login.this, "Please Enter Email", Toast.LENGTH_SHORT).show();
+                    return;
+
+                }
+                if(TextUtils.isEmpty(password)){
+                    Toast.makeText(login.this, "Please Enter Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(password.length()<6){
+                    Toast.makeText(login.this, "Password too short", Toast.LENGTH_SHORT).show();
+                }
+
+                firebaseAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(login.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    startActivity(new Intent(getApplicationContext(),Welcome.class));
+
+                                } else {
+                                    Toast.makeText(login.this, "Login Failed or User Not available", Toast.LENGTH_SHORT).show();
+
+                                }
+
+                                // ...
+                            }
+                        });
+
+
+
+            }
+        });
 
     }
 }
