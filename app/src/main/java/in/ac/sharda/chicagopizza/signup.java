33ef496b6e68cd67671c9e477a9665fd3420238d
Udit Prabhakar <uditprabhakar.up@gmail.com>
2019-11-01 23:56:52 +0530

    Sign Up Changed due to error

--- app/src/main/java/in/ac/sharda/chicagopizza/signup.java
+++ app/src/main/java/in/ac/sharda/chicagopizza/signup.java
@@ -24,94 +24,58 @@ import static com.google.firebase.auth.FirebaseAuth.getInstance;
 
 public class signup extends AppCompatActivity {
 
-    EditText txt_fullName,txt_Username,txt_email,txt_password;
-    Button btn_register;
-    RadioButton radioGenderMale, radioGenderFemale;
-    DatabaseReference databaseReference;
-    String Gender ="";
 
-    private FirebaseAuth firebaseAuth;
-
+EditText txtEmail, txtPassword, txtconfirmpassword;
+Button btn_register;
+private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        txt_fullName = (EditText)findViewById(R.id.fullname);
-        txt_Username = (EditText)findViewById(R.id.username);
-        txt_email = (EditText)findViewById(R.id.email);
-        txt_password = (EditText)findViewById(R.id.password);
-        btn_register = (Button)findViewById(R.id.register);
-        radioGenderMale = (RadioButton)findViewById(R.id.male);
-        radioGenderFemale = (RadioButton)findViewById(R.id.female);
+        txtEmail=(EditText)findViewById(R.id.email);
+        txtPassword=(EditText)findViewById(R.id.password);
+        txtconfirmpassword=(EditText)findViewById(R.id.cnfrmpassword);
+        btn_register=(Button)findViewById(R.id.register);
 
-        firebaseAuth=FirebaseAuth.getInstance();
-        databaseReference = FirebaseDatabase.getInstance().getReference("student");
-
+        firebaseAuth=firebaseAuth.getInstance();
 
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String email=txtEmail.getText().toString().trim();
+                String password=txtPassword.getText().toString().trim();
 
-                final String fullName = txt_fullName.getText().toString();
-                final String Username = txt_Username.getText().toString();
-                final String email = txt_email.getText().toString();
-                String password = txt_password.getText().toString();
 
-                if(radioGenderMale.isChecked()){
-
-                    Gender = "Male";
-                }
-                if(radioGenderFemale.isChecked()){
-                    Gender = "Female";
-                }
-
-                if(TextUtils.isEmpty(email)){
+                if (TextUtils.isEmpty(email)) {
                     Toast.makeText(signup.this, "Please Enter Email", Toast.LENGTH_SHORT).show();
-                }
-                if(TextUtils.isEmpty(Username)){
-                    Toast.makeText(signup.this, "Please Enter Username", Toast.LENGTH_SHORT).show();
-                }
-                if(TextUtils.isEmpty(fullName)){
-                    Toast.makeText(signup.this, "Please Enter FullName", Toast.LENGTH_SHORT).show();
+                    return;
                 }
                 if(TextUtils.isEmpty(password)){
                     Toast.makeText(signup.this, "Please Enter Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (password.length()<6){
+                    Toast.makeText(signup.this, "Password too short", Toast.LENGTH_SHORT).show();
                 }
                 firebaseAuth.createUserWithEmailAndPassword(email, password)
                         .addOnCompleteListener(signup.this, new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
+
                                 if (task.isSuccessful()) {
-                                    startActivity(new Intent(getApplicationContext(),login.class));
-                                    student information = new student(
-                                            fullName,
-                                            Username,
-                                            email,
-                                            Gender
-                                    );
-
-                                    FirebaseDatabase.getInstance().getReference("Customer")
-                                            .child(getInstance().getCurrentUser().getUid())
-                                            .setValue(information).addOnCompleteListener(new OnCompleteListener<Void>() {
-                                        @Override
-                                        public void onComplete(@NonNull Task<Void> task) {
-                                            Toast.makeText(signup.this, "Registration Complete", Toast.LENGTH_SHORT).show();
-                                            startActivity(new Intent(getApplicationContext() ,MainActivity.class));
-
-                                        }
-                                    });
-
+                                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                                    Toast.makeText(signup.this, "Registration Complete", Toast.LENGTH_SHORT).show();
                                 } else {
                                     Toast.makeText(signup.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
-
                                 }
 
-                                // ...
+
                             }
                         });
             }
         });
+
 
 
     }
